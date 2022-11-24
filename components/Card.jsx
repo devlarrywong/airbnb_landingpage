23a77eb7aed2dfc5e6bfdfc9ae16076a041e673b
Larry Wong <dev.larry.wong@gmail.com>
2022-11-24 22:31:01 +0000

    Card component added by array loop from data.js

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,25 +1,36 @@
 import React from 'react';
 import './Card.css';
 import starIcon from '../src/assets/star.png';
-import cardPhoto from '../src/assets/katie-zaferes.png';
 
-const Card = () => {
+const Card = (props) => {
+  let isOnline;
+  if (props.location === 'Online') {
+    isOnline = true;
+  } else {
+    isOnline = false;
+  }
+
   return (
     <div className="Card__container">
-      <div>
-        <img className="Card__photo" src={cardPhoto} />
+      <div className="Card__photo-container">
+        {isOnline && (
+          <div className="Card__photo-badge">
+            {props.openSpots === 0 ? 'Sold Out' : 'ONLINE'}
+          </div>
+        )}
+        <img className="Card__photo" src={`../src/assets/${props.coverImg}`} />
       </div>
       <div className="Card__rating-container">
         <img className="Card__cardPhoto" src={starIcon} />
-        <span className="Card__font Card__font-black">5.0</span>
-        <span className="Card__font Card__font-grey">(6)</span>
-        <span className="Card__font Card__font-grey">• USA</span>
+        <span className="Card__font Card__font-black">{props.rating}</span>
+        <span className="Card__font Card__font-grey">
+          ({props.reviewCount})
+        </span>
+        <span className="Card__font Card__font-grey">• {props.location}</span>
       </div>
-      <p className="Card__font Card__rating-black">
-        Life lessons with Katie Zaferes
-      </p>
+      <p className="Card__font Card__rating-black">{props.title}</p>
       <div className="Card__font Card__font-black Card__price-container">
-        <p className="Card__bold">From $136</p>
+        <p className="Card__bold">From ${props.price}</p>
         <p>/ person</p>
       </div>
     </div>
--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,28 @@ import './App.css';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 import Card from '../components/Card';
+import data from '../data';
 
 function App() {
-  // <Hero />
+  const cards = data.map((item) => {
+    return (
+      <Card
+        key={item.id}
+        coverImg={item.coverImg}
+        title={item.title}
+        price={item.price}
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        location={item.location}
+        openSpots={item.openSpots}
+      />
+    );
+  });
   return (
     <div className="Container">
       <Header />
-      <Card />
+      <Hero />
+      <div className="Cards__container">{cards}</div>
     </div>
   );
 }
